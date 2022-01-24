cc753020d3207b3ebc6094ee88f3324dceafffab
Bruce Bauer <blbauer@ualr.edu>
2022-01-24 16:33:08 +0000

    Unit 1

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("Please enter your first name: ")  # Input statement without a prompt
+print("You Entered: " + firstname)
+
+lastname = input("Enter Your Name: ")  # Input statement with a prompt
+print("You Entered: " + lastname)
